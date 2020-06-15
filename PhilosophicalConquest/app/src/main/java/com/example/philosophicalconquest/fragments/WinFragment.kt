bbedbf9874b0847e14ae924e160d265b5c7154bd
Papryk <gustaw5022@gmail.com>
2020-06-15 14:27:19 +0200

    changed fragments to be blank

--- PhilosophicalConquest/app/src/main/java/com/example/philosophicalconquest/fragments/WinFragment.kt
+++ PhilosophicalConquest/app/src/main/java/com/example/philosophicalconquest/fragments/WinFragment.kt
@@ -1,11 +1,14 @@
 package com.example.philosophicalconquest.fragments
 
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.philosophicalconquest.R
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,48 +17,73 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [WinFragment.newInstance] factory method to
+ * Activities that contain this fragment must implement the
+ * [AccountFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [AccountFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class WinFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
+    private var param1: Int? = null
     private var param2: String? = null
+    private var listener: OnFragmentInteractionListener? = null
+
+    private lateinit var rootView: View
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
+            param1 = it.getInt(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_win, container, false)
+        rootView = inflater.inflate(R.layout.fragment_win, container, false);
+        initFragment()
+        return rootView
     }
 
+    fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WinFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(param1: Int) =
             WinFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putInt(ARG_PARAM1, param1)
                 }
             }
+    }
+
+    private fun initFragment() {
+
     }
 }
