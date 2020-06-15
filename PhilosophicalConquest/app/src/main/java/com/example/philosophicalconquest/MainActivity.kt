4bc1888b5827f0c38e3d1c50d3749db1e67a28e9
Papryk <gustaw5022@gmail.com>
2020-06-15 13:35:34 +0200

    added main menu

--- PhilosophicalConquest/app/src/main/java/com/example/philosophicalconquest/MainActivity.kt
+++ PhilosophicalConquest/app/src/main/java/com/example/philosophicalconquest/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.philosophicalconquest
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.philosophicalconquest.fragments.MainMenuFragment
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainMenuFragment: MainMenuFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+                .beginTransaction()
+                .setCustomAnimations(
+                        R.anim.enter_bottom_to_top, R.anim.exit_top_to_bottom,
+                        R.anim.enter_top_to_bottom, R.anim.exit_bottom_to_top
+                )
+                .add(R.id.frame_layout, mainMenuFragment)
+                .commit()
     }
 }
---
+++ PhilosophicalConquest/app/src/main/java/com/example/philosophicalconquest/fragments/MainMenuFragment.kt
@@ -0,0 +1,118 @@
+package com.example.philosophicalconquest.fragments
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.example.philosophicalconquest.R
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [AccountFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [AccountFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainMenuFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private var listener: OnFragmentInteractionListener? = null
+
+    private lateinit var rootView: View
+    private lateinit var newGameButton: Button
+    private lateinit var rulesButton: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        rootView = inflater.inflate(R.layout.fragment_main_menu, container, false);
+        initFragment()
+        return rootView
+    }
+
+    fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            MainMenuFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+
+    private fun initFragment() {
+        newGameButton = rootView.findViewById(R.id.button_new_game)
+        rulesButton = rootView.findViewById(R.id.button_rules)
+
+        newGameButton.setOnClickListener {
+//            val gameSizeFragment = GameSizeFragment.newInstance()
+//            fragmentManager
+//                ?.beginTransaction()
+//                ?.setCustomAnimations(
+//                    R.anim.enter_left_to_right, R.anim.exit_right_to_left,
+//                    R.anim.enter_right_to_left, R.anim.exit_left_to_right
+//                )
+//                ?.replace(R.id.frame_layout, gameSizeFragment)
+//                ?.addToBackStack(gameSizeFragment.toString())
+//                ?.commit()
+        }
+
+        rulesButton.setOnClickListener {
+//            val rulesFragment = RulesFragment.newInstance()
+//            fragmentManager
+//                ?.beginTransaction()
+//                ?.setCustomAnimations(
+//                    R.anim.enter_left_to_right, R.anim.exit_right_to_left,
+//                    R.anim.enter_right_to_left, R.anim.exit_left_to_right
+//                )
+//                ?.replace(R.id.frame_layout, rulesFragment)
+//                ?.addToBackStack(rulesFragment.toString())
+//                ?.commit()
+        }
+    }
+}
